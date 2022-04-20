321b35740b10be5442b673f89b671986e46aa0ab
BPNguyen <bp.nguyen05@gmail.com>
2022-04-20 12:15:57 -0400

    Utilized global theme.js

--- rejig/src/Components/header.js
+++ rejig/src/Components/header.js
@@ -1,25 +1,10 @@
 import React from "react";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+import theme from "./theme.js";
+
+import { ThemeProvider } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import { Typography } from "@mui/material";
-
-const theme = createTheme({
-	palette: {
-		background: {
-			darkanthracite: "#292C33",
-		},
-		primary: {
-			main: "#1da1f2",
-			twitter: "#1da1f2",
-			spotify: "#1db954",
-		},
-		secondary: {
-			main: "#fff",
-			twitter: "#4c4e52",
-			spotify: "#fff",
-		},
-	},
-});
 
 export default function Header() {
 	return (
@@ -55,16 +40,16 @@ export default function Header() {
 					Jig
 				</Typography>
 			</Box>
-            <Box>
-                <Typography
-                    variant='h5'
-                    sx={{
-                        color: "secondary.spotify",
-                    }}    
-                >
-                    Curate a Spotify playlist from your favorite Tweet.
-                </Typography>
-            </Box>
+			<Box>
+				<Typography
+					variant='h5'
+					sx={{
+						color: "secondary.spotify",
+					}}
+				>
+					Curate a Spotify playlist from your favorite Tweet.
+				</Typography>
+			</Box>
 		</ThemeProvider>
 	);
 }
